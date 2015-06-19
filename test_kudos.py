199a7051a865dc3ed7c051dfe9807b099a1c248e
Sijis Aviles <sijis.aviles+github@gmail.com>
2015-06-18 23:46:14 -0500

    added tests

---
+++ test_kudos.py
@@ -0,0 +1,37 @@
+import kudos
+from errbot.backends.test import testbot, push_message, pop_message
+from errbot import plugin_manager
+
+
+class TestKudos(object):
+    extra_plugin_dir = '.'
+
+    def test_delete_kudos_user_empty(self, testbot):
+        push_message('!kudos delete_entries sijis')
+        assert 'User sijis has no entries' in pop_message()
+
+    def test_give_kudos(self, testbot):
+        push_message('!sijis++')
+        assert 'kudos updated for sijis' in pop_message()
+
+    def test_remove_kudos(self, testbot):
+        push_message('!sijis--')
+        assert 'Seriously...?' in pop_message()
+
+    def test_delete_kudos_user(self, testbot):
+        push_message('!sijis++')
+        assert 'kudos updated for sijis' in pop_message()
+        push_message('!kudos delete_entries sijis')
+        assert 'Entries deleted for sijis user' in pop_message()
+
+    def test_delete_kudos_list(self, testbot):
+        push_message('!sijis++')
+        assert 'kudos updated for sijis' in pop_message()
+        push_message('!tom++')
+        assert 'kudos updated for tom' in pop_message()
+        push_message('!kudos list')
+        assert 'sijis, tom' in pop_message()
+
+    def test_kudos_stats(self, testbot):
+        push_message('!kudos sijis')
+        assert 'sijis has 1 kudo points' in pop_message()
